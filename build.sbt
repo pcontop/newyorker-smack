c54409b6d3f40d79d525c75dc39900842e3e82c5
pcontop <pcontopoulos@gmail.com>
2019-05-12 18:48:00 -0300

    Initial commit.

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "NewYorker"
+
+version := "0.1"
+
+scalaVersion := "2.11.9"
+
+//Spark
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.3"
+
+//Compress
+libraryDependencies += "org.apache.commons" % "commons-compress" % "1.11"
+
+//json
+libraryDependencies += "io.argonaut" %% "argonaut" % "6.2.2"
---
+++ src/main/scala/Reader.scala
@@ -0,0 +1,50 @@
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.spark.input.PortableDataStream
+
+import scala.util.Try
+import java.nio.charset._
+
+import argonaut.Parse
+import org.apache.spark.SparkContext
+
+object Reader {
+  def extractFiles(ps: PortableDataStream, n: Int = 1024) = Try {
+    val tar = new TarArchiveInputStream(ps.open)
+    Stream.continually(Option(tar.getNextTarEntry))
+      // Read until next exntry is null
+      .takeWhile(_.isDefined)
+      // flatten
+      .flatten
+      // Drop directories
+      .filter(!_.isDirectory)
+      .map(e => {
+        Stream.continually {
+          // Read n bytes
+          val buffer = Array.fill[Byte](n)(-1)
+          val i = tar.read(buffer, 0, n)
+          (i, buffer.take(i))}
+          // Take as long as we've read something
+          .takeWhile(_._1 > 0)
+          .flatMap(_._2)
+          .toArray})
+      .toArray
+  }
+
+  def decode(charset: Charset = StandardCharsets.UTF_8)(bytes: Array[Byte]) =
+    new String(bytes, StandardCharsets.UTF_8)
+
+  def main(args: Array[String]): Unit = {
+    val pathToFile = args(0)
+
+    val sc = new SparkContext()
+
+    val filesAsText = sc.binaryFiles(pathToFile).flatMapValues(x =>
+      extractFiles(x).toOption).mapValues(_.map(decode())).flatMap(_._2)
+    //No error treatment for now.
+    val filesAsJson = filesAsText.map(Parse.parseOption(_)).filter(_.nonEmpty)
+
+
+  }
+
+}
